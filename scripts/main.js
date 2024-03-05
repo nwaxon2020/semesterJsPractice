35c89071b75dbd7940c3d80fb4e2db8e72e8aa27
Prince Nwachukwu <princenwachukwu308@yahoo.com>
2024-03-05 19:18:11 +0100

    visit Done

--- scripts/main.js
+++ scripts/main.js
@@ -13,3 +13,28 @@ home.addEventListener("click", () => {
     }
 
 })
+
+//////////////////// Last Visited //////////////////////
+const lastVisit = document.querySelector("#lastvisit");
+let numTimes = 0;
+
+let dateVisit = new Date();
+
+document.addEventListener("DOMContentLoaded", () => {
+
+
+    numTimes = localStorage.getItem("numTimes");
+
+    numTimes++;
+    if (numTimes <= 1) {
+        lastVisit.innerHTML = ` Welcome, Visit-Time: ${numTimes}<br>${dateVisit}`;
+        localStorage.setItem("numTimes", numTimes);
+    } else if (dateVisit.getHours() < 24) {
+        lastVisit.innerHTML = `Back So soon!! ${numTimes}<br>${dateVisit}`;
+        localStorage.setItem("numTimes", numTimes);
+    } else {
+        lastVisit.innerHTML = `Visit-Times: ${numTimes}<br>${dateVisit}`;
+        localStorage.setItem("numTimes", numTimes);
+    }
+
+});
